ba5342c1a2cff6fa31b991ab72c7ae38ad9c1666
JusRecondo <justina.recondo@gmail.com>
2024-12-09 12:39:07 -0300

    feat: add PokemonDetail component

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { Route, Routes } from "react-router"
 import Home from "./pages/Home"
 import Header from "./components/Header"
+import PokemonDetail from "./components/PokemonDetail"
 
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
       <Header />
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/pokemon/:id" element={<PokemonDetail />} />
       </Routes>
     </>
   )
---
+++ src/components/PokemonDetail.jsx
@@ -0,0 +1,57 @@
+import { useDispatch, useSelector } from "react-redux"
+import { useNavigate, useParams } from "react-router"
+import { fetchPokemonById, getPokemonLoading, selectPokemon } from "../redux/features/pokemonSlice"
+import { useEffect } from "react"
+import { BiArrowBack } from "react-icons/bi"
+
+const PokemonDetail = () => {
+  const params = useParams()
+  const navigate = useNavigate()
+  const dispatch = useDispatch()
+  const pokemonDetails = useSelector(selectPokemon)
+  const loading = useSelector(getPokemonLoading)
+
+  useEffect(() => {
+    dispatch(fetchPokemonById(params.id))
+  }, [])
+
+  return (
+    <main id="pokemon-detail">
+      {
+        loading && (
+          <p>Loading...</p>
+        )
+      }
+      <button onClick={() => navigate(-1)} className="go-back" aria-label="go back to home page">
+        <BiArrowBack />
+      </button>
+      <section>
+        <div className="section-inner">
+          <header>
+            <h2>{pokemonDetails?.name}</h2>
+            <p>
+              <strong>Types:</strong> {pokemonDetails?.types}
+            </p>
+          </header>
+          <img 
+            src={pokemonDetails.img ? pokemonDetails.img : '/pokeball.svg'} 
+            alt={pokemonDetails?.name} 
+          />
+            <p><strong>Abilities:</strong> {pokemonDetails?.abilities}</p>
+          <div className="pokemon-stats">
+            <h3>Stats</h3>
+            <ul>
+              {pokemonDetails?.stats?.map((stat, i) => (
+                <li key={i}>
+                  <strong>{stat.name}:</strong> {stat.value}
+                </li>
+              ))}
+            </ul>
+          </div>
+        </div>
+      </section>
+    </main>
+  )
+}
+
+export default PokemonDetail
