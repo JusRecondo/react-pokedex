0c95c2c31ac1dd16f293d5dd3470b31111c8434f
JusRecondo <justina.recondo@gmail.com>
2024-12-10 10:23:52 -0300

    feat: add input in Header component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,22 @@
+import useSearch from "../hooks/useSearch"
+
 const Header = () => {
+  const { handleChange, handleSubmit, search } = useSearch()
+
   return (
     <header id="main-header">
         <h1>Pokemon</h1>
+        <form onSubmit={handleSubmit}>
+          <input 
+            type="text" 
+            name="search" 
+            id="search" 
+            value={search} 
+            onChange={handleChange} 
+            placeholder="Search for a Pokemon"
+            aria-label="Search for a Pokemon"
+          />
+        </form>
     </header>
   )
 }
